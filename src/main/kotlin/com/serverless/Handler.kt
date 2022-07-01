27ff592caa2c6f609110cf08f0cd92b94f4fae4e
C0285129 <dimitris.charilas@santander.co.uk>
2022-07-01 14:51:10 +0300

    Adds example for environment variable

--- src/main/kotlin/com/serverless/Handler.kt
+++ src/main/kotlin/com/serverless/Handler.kt
@@ -6,10 +6,12 @@ import org.apache.logging.log4j.LogManager
 import java.util.*
 
 class Handler:RequestHandler<Map<String, Any>, ApiGatewayResponse> {
+
+    var envVar: String = System.getenv("APP_ENV_VARIABLE") ?: "Serverless"
+
     override fun handleRequest(input:Map<String, Any>, context:Context):ApiGatewayResponse {
         LOG.info("received: " + input.keys.toString())
-
-        val responseBody = Response("Go Serverless v1.x! Your Kotlin function executed successfully!", input)
+        val responseBody = Response("Go $envVar! Your Kotlin function executed successfully!", input)
         return ApiGatewayResponse.build {
             statusCode = 200
             objectBody = responseBody
